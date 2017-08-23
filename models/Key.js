997b7bd7da44f98f9b5357650de77bfa686d8710
Shawn Gavaghan Jr <shawn312.eve@gmail.com>
2017-08-23 19:22:21 -0400

    .gitignore is now working

--- models/Key.js
+++
@@ -1,7 +0,0 @@
-const Key = {
- CLIENT_ID: '584bde30bb1a4d5aa4735571f13a9cfc',
- SECRET_KEY: 'oKYCkHVVtFRmvPAMJry7vkL3FeFhyZgFZLqHUEop',
- CALLBACK_URL: 'http://localhost:8000/key',
-};
-
-module.exports = Key;
